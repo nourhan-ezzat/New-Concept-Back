696ca811cc9155bd456db40ea1642534d2a44fb2
nourhan-ezzat <nourhanezzat64@gmail.com>
2019-07-30 23:24:58 +0200

    First node app deploy

--- src/index.js
+++ src/index.js
@@ -6,12 +6,10 @@ const app = express()
 
 const UserRouter = require('./routers/user')
 
-const port = process.env.PORT || 3001
-
 app.use(express.json())
 app.use(cors())
 app.use(UserRouter)
 
-app.listen(port, () =>{
-    console.log('Server is up on port ' + port)
-})
+app.listen(process.env.PORT || 3001, function(){
+    console.log('Your node js server is running');
+});
