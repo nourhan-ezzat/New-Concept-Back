62550e0a6f9fe2d78f1be3d20ab7c996e58a958b
nourhan-ezzat <nourhanezzat64@gmail.com>
2019-08-10 03:16:41 +0200

    task front

--- src/index.js
+++ src/index.js
@@ -10,6 +10,11 @@ app.use(express.json())
 app.use(cors())
 app.use(UserRouter)
 
-app.listen(process.env.PORT || 3001, function(){
-    console.log('Your node js server is running');
-});
+app.get('/', function(req, res) {
+    res.send('hello');
+})
+
+const port = process.env.PORT || 3001
+app.listen(port, () =>{
+    console.log('Server is up on port ' + port)
+})
